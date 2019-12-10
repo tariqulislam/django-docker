4b95a4e91e3c6eb4749917cdf9b3f2dea6f64ada
mohamis2 <mohamis2@cisco.com>
2019-12-10 18:19:36 +0900

    add the template folder to list1

--- app/main/views.py
+++ app/main/views.py
@@ -8,7 +8,7 @@ from .forms import CompanyForm, EmployeeForm
 class CompanyListView(View):
     template_name = 'list.html'
 
-class CompanyView(View):
+class CompanyCreateView(View):
     form_class = CompanyView
     inital = {}
     template_name = 'company.html'
@@ -23,15 +23,28 @@ class CompanyView(View):
             return HttpResponseRedirect('/success')
         return render(request, self.template_name, {'form': form})
 
+
+class CompanyEditFormView(CompanyCreateView):
+    
+    def get(self, request, id, *args, **kwargs):
+        form = self.form_class(request)
+    
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(initial=request.POST)
+        
+        if form.is_valid():
+            return HttpResponseRedirect('/success')
+        return render(request, self.template_name, {'form': form})
+
 class EmployeeListView(View):
     form_class = EmployeeForm
     initial = {}
-    template_name = 'list.html'
+    template_name = 'employee/employee_list.html'
 
-class EmployeeFormView(View):
+class EmployeeCreateFormView(View):
     form_class = EmployeeForm
     initial = {}
-    template_name = 'employee.html'
+    template_name = 'employee/employee_edit.html'
 
     def get(self, request, *args, **kwargs):
         form = self.form_class(initial=initial)
@@ -39,6 +52,18 @@ class EmployeeFormView(View):
     
     def post(self, request, *args, **kwargs):
         form = self.form_class(initial=self.initial)
+        if form.is_valid():
+            return HttpResponseRedirect('/success')
+        return render(request, self.template_name, {'form': form})
+
+class EmployeeEditFormView(EmployeeCreateFormView):
+    
+    def get(self, request, id, *args, **kwargs):
+        form = self.form_class(request)
+    
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(initial=request.POST)
+        
         if form.is_valid():
             return HttpResponseRedirect('/success')
         return render(request, self.template_name, {'form': form})
